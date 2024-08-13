28356960f3e4e1b79bda24d643bf651af76278c7
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2024-08-13 16:19:56 -0300

    Update Ex002.java

--- Ex002.java
+++ Ex002.java
@@ -1,3 +1,20 @@
+/*02 (Calculadora)   
+
+Escreva um programa que solicite ao usuário dois números inteiros, e realize as operações de soma, subtração, divisão e multiplicação. Ao final, exiba o resultado
+*/
+
+import java.util.Scanner;
 public class Ex002 {
-    
+	public static void main(String[] args){
+		Scanner entrada1 = new Scanner(System.in);
+		Scanner entrada2 = new Scanner(System.in);
+
+		int num1 = entrada1.nextInt();
+		int num2 = entrada2.nextInt();
+
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+	}
 }
