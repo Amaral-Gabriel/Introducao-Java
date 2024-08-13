53f0797e4f48791e7626801a450e9fdf83546522
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2024-08-13 16:15:21 -0300

    Ex001

---
+++ Ex001.java
@@ -0,0 +1,17 @@
+/*
+01 (Casting)  
+Faça um programa que solicite ao usuário um número decimal, converta-o para inteiro e exiba o resultado.
+*/
+
+import java.util.Scanner;
+public class Ex001 {
+	public static void main(String[] args){
+		Scanner entrada = new Scanner(System.in);
+
+		double decimal = entrada.nextDouble();
+
+		int inteiro = (int) decimal;
+
+		System.out.println(inteiro);
+	}
+}
