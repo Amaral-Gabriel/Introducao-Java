15b1732f5ad954e7393444bc5529eea04de64c3e
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2024-08-23 11:56:38 -0300

    Create ExL6006.java

---
+++ ExL6006.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class ExL6006 {
+    public static void main(String[] args) {
+        HashMap<String, String> carros = new HashMap<String, String>();
+
+        carros.put("Renault", "França");
+        carros.put("Toyota", "Japão");
+        carros.put("Chevrolet","EUA");
+        carros.put("Fiat", "Italia");
+
+        System.out.println(carros);
+
+    }
+}
